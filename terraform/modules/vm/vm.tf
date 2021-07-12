2cc383e560cf3440526a57971f263700ac627f76
vigneshgd <52568208+vigneshgd@users.noreply.github.com>
2021-07-12 17:18:31 -0400

    changed key in vm create

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -21,14 +21,15 @@ resource "azurerm_linux_virtual_machine" "test" {
   resource_group_name  = "${var.resource_group}"
   size                 = "${var.vm_size}"
   admin_username       = "${var.admin_user}"
+
   disable_password_authentication = "true"
   network_interface_ids = [
     azurerm_network_interface.test.id,
     ]
   admin_ssh_key {
     username       = "${var.admin_user}"
-    public_key = file("/home/vsts/work/_temp/udacity_azure.pub")
-#    public_key = "${var.vm_ssh_key}"
+#    public_key = file("/home/vsts/work/_temp/udacity_azure.pub")
+    public_key = "qualityrelease AAAAB3NzaC1yc2EAAAABIwAAAQEArIood27UiDWIquKG6eT/vaMeCdVdcIEnzFn9UAMCd9RRQrESQKXSpMhpJWA9KDOOlHKG2R/uqDqv2z4hxRy3vCyTj+J9EnWnoFvPkiQijCQ4K+KcF9dEL7EP4WQD9H5eYGl0HyoKx6vzSBt3ZQydWOOfTdCGQueSpjp2b+hpFZvg6D54EVtCXzdQhkMTL30ehPZjdCwu6M2dzOILGJpfathb4S1DzTVaerujm3uPTCtfchOkQJb4TUFbraU56vb9fzs7HHh2hWOCvrkgRAkBiqO2BwdS+vRvfxN9l3u1j99n3gBrxDkDqJ32FZ7iTif77sA1c57j2dnIs3LUS3U6kw== qualityrelease"
   }
   os_disk {
     caching           = "ReadWrite"
